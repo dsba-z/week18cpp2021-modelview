462fba34d14fe30592fd35a60e02ca4c66ff39d3
Georgii Zhulikov <l8doku@gmail.com>
2022-05-31 14:39:22 +0300

    Workshop 36

--- src/ex_modelview/mainwindow.cpp
+++ src/ex_modelview/mainwindow.cpp
@@ -9,31 +9,41 @@ MainWindow::MainWindow(QWidget *parent)
     
     exampleModel = new ExampleModel(this);
     
-//    exampleModel->fillDataTableFromFile(fileName);
-    
     ui->setupUi(this);
     ui->tableView->setModel(exampleModel);
     
-    
     QObject::connect(ui->pushButton, SIGNAL(clicked()), this, SLOT(loadFileSlot()));
-    
+//    QObject::connect(ui->actionAbout, SIGNAL(triggered()), this, SLOT(loadFileSlot()));
+    QObject::connect(ui->tableView, SIGNAL(clicked(QModelIndex)), this, SLOT(changeCurrentRowSlot(QModelIndex)));
 }
 
 void MainWindow::loadFileSlot()
 {
-    QString fileName = QFileDialog::getOpenFileName(this, "Open File",
-                                                    "/home/georgii/WORK",
-                                                    "Images (*.csv)");
-//    exampleModel->fillDataTable();
+    QString fileName = QFileDialog::getOpenFileName(this, "Open File", "../", "Images (*.csv)");
     delete exampleModel;
     exampleModel = new ExampleModel(this);
     exampleModel->fillDataTableFromFile(fileName);
     ui->tableView->setModel(exampleModel);
+}
 
+void MainWindow::changeCurrentRowSlot(const QModelIndex &index) // also add this to mainwindow.h
+{
+    
+    int row = index.row();
+    int column = 5;
+    
+    QModelIndex ageIndex = exampleModel->index(row, column);
+    QVariant ageData = exampleModel->data(ageIndex);
+    ui->ageLabel->setText(ageData.toString());
+    
+    
+    QModelIndex dataIndex = exampleModel->index(row, 10);
+    QVariant dataData = exampleModel->data(dataIndex);
+    ui->dataLabel->setText(dataData.toString());
+    
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
-
--- src/ex_modelview/mainwindow.h
+++ src/ex_modelview/mainwindow.h
@@ -19,6 +19,7 @@ public:
 private slots:
     void loadFileSlot();
     
+    
 private:
     Ui::MainWindow *ui;
     ExampleModel *exampleModel;
